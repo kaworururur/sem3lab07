2d1039efb254469e1e0631ad004fc66b4ab94378
kaworururur <elizabethbump2005@gmail.com>
2024-12-26 20:29:56 +0300

    lab07

---
+++ lab07/AnimalLibrary.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace AnimalLibrary
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    //определяет пользовательский атрибут CommentAtt и указывает, что атрибут может применяться только к классам (AttributeTargets.Class)
+    public class CommentAtt : Attribute
+    {
+        public string Comment { get; set; }
+
+        public CommentAtt(string comment)
+        {
+            Comment = comment;
+        }
+    }
+
+    public enum AnimalClassification
+    //определяется перечисление AnimalClassification, которое представляет классификацию животных - травоядные, хищники, всеядные
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum FavouriteFood
+    //определяется перечисление FavouriteFood, которое представляет любимую пищу животных - мясо, растения, все
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [CommentAtt("Абстрактный класс для объектов, представляющих животных")]
+    public abstract class Animal
+    {
+        private AnimalClassification classification;
+
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public bool HideFromOtherAnimals { get; set; }
+
+        public abstract void SayHello();
+        public abstract FavouriteFood GetFavouriteFood();
+
+        public Animal(string country, string name, string description, bool hideFromOtherAnimals)
+        {
+            Name = name;
+            Country = country;
+            Description = description;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+        }
+
+        public void Deconstruct(out string out_name)
+        {
+            out_name = Name;
+        }// возвращает значение свойства Name через параметр out_name
+
+        public void Deconstruct(out string out_name, out string out_desc)
+        {
+            out_name = Name;
+            out_desc = Description;
+        }
+
+        public void Deconstruct(out string out_name, out string out_desc, out string out_count)
+        {
+            out_name = Name;
+            out_count = Country;
+            out_desc = Description;
+        }
+
+        public void Deconstruct(out string out_name, out string out_desc, out string out_count, out bool out_hide)
+        {
+            out_name = Name;
+            out_count = Country;
+            out_desc = Description;
+            out_hide = HideFromOtherAnimals;
+        }
+    }
+
+    [CommentAtt("Класс описания коровы")]
+    public class Cow : Animal
+    {
+        public Cow(string country, string name, string description, bool hideFromOtherAnimals) :
+            base(country, name, description, hideFromOtherAnimals)
+        {
+        }
+
+        public override FavouriteFood GetFavouriteFood()
+        {
+            return FavouriteFood.Plants;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Moo\n");
+        }
+    }
+
+    [CommentAtt("Класс описания льва")]
+    public class Lion : Animal
+    {
+        public Lion(string country, string name, string description, bool hideFromOtherAnimals) :
+            base(country, name, description, hideFromOtherAnimals)
+        {
+        }
+
+        public override FavouriteFood GetFavouriteFood()
+        {
+            return FavouriteFood.Meat;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Roar\n");
+        }
+    }
+
+    [CommentAtt("Класс описания свиньи")]
+    public class Pig : Animal
+    {
+        public Pig(string country, string name, string description, bool hideFromOtherAnimals) :
+            base(country, name, description, hideFromOtherAnimals)
+        {
+        }
+
+        public override FavouriteFood GetFavouriteFood()
+        {
+            return FavouriteFood.Everything;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Oink\n");
+        }
+    }
+}
---
+++ lab07/Program.cs
@@ -0,0 +1,152 @@
+﻿using Sem3Lab7;
+using System.Reflection;
+using System.Xml.Linq;
+class Program
+{
+    static void Main()
+    {
+        var assembly = Assembly.GetAssembly(typeof(Animal));
+        var types = assembly.GetTypes().Where(t => t.IsClass && t.Namespace == "Sem3Lab7");
+        var xml = new XElement("Classes", types.Select(type => new XElement("Class", new XAttribute("Name", type.Name), new XAttribute("Comment", GetComment(type)), new XElement("Methods", type.GetMethods(BindingFlags.Public | BindingFlags.Instance).Select(m => new XElement("Method", m.Name))))));
+        File.WriteAllText("ClassDiagram.xml", xml.ToString());
+        Console.WriteLine("xml generated");
+    }
+    static string GetComment(Type type)
+    {
+        var attribute = type.GetCustomAttributes(typeof(AttributeCommentforClass), false).FirstOrDefault() as AttributeCommentforClass;
+        return attribute?.Comment ?? string.Empty; // "no comment"?
+    }
+}
+
+namespace Sem3Lab7
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class AttributeCommentforClass : Attribute
+    {
+        public string Comment { get; set; }
+        public AttributeCommentforClass(string comment)
+        {
+            Comment = comment;
+        }
+    }
+
+
+
+    [AttributeCommentforClass("Класс всех животных")]
+    public abstract class Animal
+    {
+        private eClassificationAnimal Classification;
+
+        public int Country
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int HideFromOtherAnimals
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int WhatAnimal
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Animal() { }
+        public void Deconstruct()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetClassificationAnimal()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetFavouriteFood()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SayHello()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [AttributeCommentforClass("Класс с представлением коровы")]
+    public class Cow : Animal
+    {
+        public Cow() { }
+        public void GetFavouriteFood()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello Cow");
+            throw new System.NotImplementedException();
+        }
+    }
+    [AttributeCommentforClass("Класс с представлением льва")]
+    public class Lion : Animal
+    {
+        public Lion() { }
+        public void GetFavouriteFood()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SayHello()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    [AttributeCommentforClass("Класс с представлением свиньи")]
+    public class Pig : Animal
+    {
+        public Pig() { }
+        public void GetFavouriteFood()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SayHello()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+}
